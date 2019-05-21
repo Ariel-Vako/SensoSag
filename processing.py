172408c5ddb0924c81cd851e352cfb0017a48fae
ariel <vmard001@gmail.com>
2019-05-21 11:19:03 -0400

    Initial commit

---
+++ processing.py
@@ -0,0 +1,19 @@
+"""processing.py: Denoising sensor data with wavelettes (MRA), it collects features to train a machine learning algorithm.
+                  Finally, calculate the tail angle depends on classification"""
+
+__autor__ = 'Ariel Mardones'
+__copyright__ = 'Copyright 2019, Highservice'
+__credits__ = ['Guillermo Vidal', 'José Sanhueza', 'Ariel Mardones']
+
+__version__ = '1.0.0'
+__date__ = '2019-05-21'
+__email__ = 'amardones@highservice.cl'
+
+__source__ = 'http://ataspinar.com/2018/12/21/a-guide-for-using-the-wavelet-transform-in-machine-learning/'
+
+data  # Resultado de la query transformada de unicode
+
+# Segmentación de señal por intervalos de tiempo coherentes entre ellos
+# Código guillermo
+
+#
