6506a1acf4e6b547a44813a1856d19778acc4b43
Ariel <44302137+Ariel-Vako@users.noreply.github.com>
2019-05-23 12:32:30 -0400

    Update processing.py

--- processing.py
+++ processing.py
@@ -38,15 +38,69 @@ def grafica(signal, ciclo, reconstructed_signal):
     ax.set_xlabel(f'Cicle {ciclo}', fontsize=16)
     plt.show()
     return fig
+ 
+def calculate_entropy(list_values):
+    counter_values = Counter(list_values).most_common()
+    probabilities = [elem[1]/len(list_values) for elem in counter_values]
+    entropy=scipy.stats.entropy(probabilities)
+    return entropy
+ 
+def calculate_statistics(list_values):
+    n5 = np.nanpercentile(list_values, 5)
+    n25 = np.nanpercentile(list_values, 25)
+    n75 = np.nanpercentile(list_values, 75)
+    n95 = np.nanpercentile(list_values, 95)
+    median = np.nanpercentile(list_values, 50)
+    mean = np.nanmean(list_values)
+    std = np.nanstd(list_values)
+    var = np.nanvar(list_values)
+    rms = np.nanmean(np.sqrt(list_values**2))
+    return [n5, n25, n75, n95, median, mean, std, var, rms]
+ 
+def calculate_crossings(list_values):
+    zero_crossing_indices = np.nonzero(np.diff(np.array(list_values) &gt; 0))[0]
+    no_zero_crossings = len(zero_crossing_indices)
+    mean_crossing_indices = np.nonzero(np.diff(np.array(list_values) &gt; np.nanmean(list_values)))[0]
+    no_mean_crossings = len(mean_crossing_indices)
+    return [no_zero_crossings, no_mean_crossings]
+ 
+def get_features(list_values):
+    entropy = calculate_entropy(list_values)
+    crossings = calculate_crossings(list_values)
+    statistics = calculate_statistics(list_values)
+    return [entropy] + crossings + statistics
 
+def get_sensosag_features(ecg_data, ecg_labels, waveletname):
+    list_features = []
+    list_unique_labels = list(set(ecg_labels))
+    list_labels = [list_unique_labels.index(elem) for elem in ecg_labels]
+    for signal in ecg_data:
+        list_coeff = pywt.wavedec(signal, waveletname)
+        features = []
+        for coeff in list_coeff:
+            features += get_features(coeff)
+        list_features.append(features)
+    return list_features, list_labels
 
+  
+  
 def fundamental(reconstructed_signal):
     return coseno
-
 
 consulta = myz.consulta_acellz(params.startDate, params.endDate, params.cantidad)
 
 data  # Resultado de la query transformada de unicode
+
+#  Split the data between train and test
+data_ecg, labels_ecg = load_ecg_data(filename)
+training_size = int(0.6*len(labels_ecg))
+train_data_ecg = data_ecg[:training_size]
+test_data_ecg = data_ecg[training_size:]
+train_labels_ecg = labels_ecg[:training_size]
+test_labels_ecg = labels_ecg[training_size:]
+
+
+
 
 tiempo = data['tiempo'].values
 accelz = data['accelz'].values
