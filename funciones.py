95f4240e1eba45f0985b4d5358fced1fe3b69baa
Ariel Mardones <vmard001@gmail.com>
2019-05-24 14:17:59 -0400

    Se crea script con funciones para, consulta, limpieza y selección de datos.

---
+++ funciones.py
@@ -0,0 +1,97 @@
+import numpy as np
+import pywt
+import matplotlib.pyplot as plt
+import MySQLdb
+import scipy
+import collections
+
+
+
+def lowpassfilter(signal, thresh=0.63, wavelet="sym7"):
+    thresh = thresh * np.nanmax(signal)
+    coeff = pywt.wavedec(signal, wavelet, mode="per")
+    coeff[1:] = (pywt.threshold(i, value=thresh, mode="soft") for i in coeff[1:])
+    reconstructed_signal = pywt.waverec(coeff, wavelet, mode="per")
+    return reconstructed_signal
+
+
+def grafica(signal, ciclo, reconstructed_signal):
+    plt.close('all')
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.plot(signal, color="b", alpha=0.5, label='original signal')
+    rec = reconstructed_signal
+    ax.plot(rec, 'k', label='DWT smoothing}', linewidth=2)
+    ax.legend()
+    ax.set_title('Removing High Frequency Noise with DWT', fontsize=18)
+    ax.set_ylabel('Signal Amplitude', fontsize=16)
+    ax.set_xlabel(f'Cicle {ciclo}', fontsize=16)
+    plt.show()
+    return fig
+
+
+def calculate_entropy(list_values):
+    counter_values = collections.Counter(list_values).most_common()
+    probabilities = [elem[1] / len(list_values) for elem in counter_values]
+    entropy = scipy.stats.entropy(probabilities)
+    return entropy
+
+
+def calculate_statistics(list_values):
+    n5 = np.nanpercentile(list_values, 5)
+    n25 = np.nanpercentile(list_values, 25)
+    n75 = np.nanpercentile(list_values, 75)
+    n95 = np.nanpercentile(list_values, 95)
+    median = np.nanpercentile(list_values, 50)
+    mean = np.nanmean(list_values)
+    std = np.nanstd(list_values)
+    var = np.nanvar(list_values)
+    rms = np.nanmean(np.sqrt(list_values ** 2))
+    return [n5, n25, n75, n95, median, mean, std, var, rms]
+
+
+def calculate_crossings(list_values):
+    zero_crossing_indices = np.nonzero(np.diff(np.array(list_values) > 0))[0]
+    no_zero_crossings = len(zero_crossing_indices)
+    mean_crossing_indices = np.nonzero(np.diff(np.array(list_values) > np.nanmean(list_values)))[0]
+    no_mean_crossings = len(mean_crossing_indices)
+    return [no_zero_crossings, no_mean_crossings]
+
+
+def get_features(list_values):
+    entropy = calculate_entropy(list_values)
+    crossings = calculate_crossings(list_values)
+    statistics = calculate_statistics(list_values)
+    return [entropy] + crossings + statistics
+
+
+def get_sensosag_features(ecg_data, ecg_labels, waveletname):
+    list_features = []
+    list_unique_labels = list(set(ecg_labels))
+    list_labels = [list_unique_labels.index(elem) for elem in ecg_labels]
+    for signal in ecg_data:
+        list_coeff = pywt.wavedec(signal, waveletname)
+        features = []
+        for coeff in list_coeff:
+            features += get_features(coeff)
+        list_features.append(features)
+    return list_features, list_labels
+
+
+def consulta_acellz(startDate, endDate, cantidad=5000):
+    db = MySQLdb.connect("hstech.sinc.cl", "jsanhueza", "Hstech2018.-)", "ssi_mlp_sag2")
+    cursor = db.cursor()
+
+    cursor.execute(f"SELECT dataZ , fecha_reg \
+        FROM Data_Sensor \
+        WHERE (id_sensor_data IN (1) AND estado_data = 134217727 \
+        AND (fecha_reg BETWEEN {startDate} AND {endDate}) ) \
+        ORDER BY fecha_reg ASC \
+        LIMIT {cantidad}")
+
+    results = cursor.fetchall()
+    db.close()
+    return results
+
+
+def fundamental(reconstructed_signal):
+    return coseno
--- processing.py
+++ processing.py
@@ -11,90 +11,11 @@ __email__ = 'amardones@highservice.cl'
 
 __source__ = 'http://ataspinar.com/2018/12/21/a-guide-for-using-the-wavelet-transform-in-machine-learning/'
 
-import numpy as np
-import pywt
-import matplotlib.pyplot as plt
-import mysql_query as myz
-import scipy
-import collections
 import params
+import funciones as fx
+import numpy as np
 
-
-def lowpassfilter(signal, thresh=0.63, wavelet="sym7"):
-    thresh = thresh * np.nanmax(signal)
-    coeff = pywt.wavedec(signal, wavelet, mode="per")
-    coeff[1:] = (pywt.threshold(i, value=thresh, mode="soft") for i in coeff[1:])
-    reconstructed_signal = pywt.waverec(coeff, wavelet, mode="per")
-    return reconstructed_signal
-
-
-def grafica(signal, ciclo, reconstructed_signal):
-    plt.close('all')
-    fig, ax = plt.subplots(figsize=(12, 8))
-    ax.plot(signal, color="b", alpha=0.5, label='original signal')
-    rec = reconstructed_signal
-    ax.plot(rec, 'k', label='DWT smoothing}', linewidth=2)
-    ax.legend()
-    ax.set_title('Removing High Frequency Noise with DWT', fontsize=18)
-    ax.set_ylabel('Signal Amplitude', fontsize=16)
-    ax.set_xlabel(f'Cicle {ciclo}', fontsize=16)
-    plt.show()
-    return fig
-
-
-def calculate_entropy(list_values):
-    counter_values = collections.Counter(list_values).most_common()
-    probabilities = [elem[1] / len(list_values) for elem in counter_values]
-    entropy = scipy.stats.entropy(probabilities)
-    return entropy
-
-
-def calculate_statistics(list_values):
-    n5 = np.nanpercentile(list_values, 5)
-    n25 = np.nanpercentile(list_values, 25)
-    n75 = np.nanpercentile(list_values, 75)
-    n95 = np.nanpercentile(list_values, 95)
-    median = np.nanpercentile(list_values, 50)
-    mean = np.nanmean(list_values)
-    std = np.nanstd(list_values)
-    var = np.nanvar(list_values)
-    rms = np.nanmean(np.sqrt(list_values ** 2))
-    return [n5, n25, n75, n95, median, mean, std, var, rms]
-
-
-def calculate_crossings(list_values):
-    zero_crossing_indices = np.nonzero(np.diff(np.array(list_values) > 0))[0]
-    no_zero_crossings = len(zero_crossing_indices)
-    mean_crossing_indices = np.nonzero(np.diff(np.array(list_values) > np.nanmean(list_values)))[0]
-    no_mean_crossings = len(mean_crossing_indices)
-    return [no_zero_crossings, no_mean_crossings]
-
-
-def get_features(list_values):
-    entropy = calculate_entropy(list_values)
-    crossings = calculate_crossings(list_values)
-    statistics = calculate_statistics(list_values)
-    return [entropy] + crossings + statistics
-
-
-def get_sensosag_features(ecg_data, ecg_labels, waveletname):
-    list_features = []
-    list_unique_labels = list(set(ecg_labels))
-    list_labels = [list_unique_labels.index(elem) for elem in ecg_labels]
-    for signal in ecg_data:
-        list_coeff = pywt.wavedec(signal, waveletname)
-        features = []
-        for coeff in list_coeff:
-            features += get_features(coeff)
-        list_features.append(features)
-    return list_features, list_labels
-
-
-def fundamental(reconstructed_signal):
-    return coseno
-
-
-consulta = myz.consulta_acellz(params.startDate, params.endDate, params.cantidad)
+consulta = fx.consulta_acellz(params.startDate, params.endDate, params.cantidad)
 
 data  # Resultado de la query transformada de unicode
 
@@ -124,8 +45,8 @@ for i in np.arange(n - 1):
 cont = 0
 while cont <= len(contador_ciclo) and (tecla == ''):
     signal = accelz[cont]
-    rec = lowpassfilter(signal, params.thresh, params.wavelet_name)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
-    grafica(signal, cont, rec)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
+    rec = fx.lowpassfilter(signal, params.thresh, params.wavelet_name)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
+    fx.grafica(signal, cont, rec)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
 
     cont += 1
     tecla = input()
