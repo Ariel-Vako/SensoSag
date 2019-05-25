71666dd891a6e8c5f21b3d432147f17a3e98aac9
ariel <vmard001@gmail.com>
2019-05-24 21:35:07 -0400

    Merge branch 'master' of https://github.com/Ariel-Vako/SensoSag
    # Conflicts:
    #	processing.py

--- processing.py
+++ processing.py
@@ -1,5 +1,5 @@
-"""processing.py: Denoising sensor data with wavelettes (MRA), it collects features to train a machine learning algorithm.
-                  Finally, calculate the tail angle depends on classification"""
+"""processing.py: Denoising sensor data with wavelettes (MRA), it collects features to train a machine
+                  learning algorithm. Finally, it calculate the tail angle depending the classification"""
 
 __autor__ = 'Ariel Mardones'
 __copyright__ = 'Copyright 2019, Highservice'
@@ -11,27 +11,42 @@ __email__ = 'amardones@highservice.cl'
 
 __source__ = 'http://ataspinar.com/2018/12/21/a-guide-for-using-the-wavelet-transform-in-machine-learning/'
 
+import params
+import funciones as fx
 import numpy as np
-import pywt
+
+consulta = fx.consulta_acellz(params.startDate, params.endDate, params.cantidad)
 
 data  # Resultado de la query transformada de unicode
 
-tiempo = data['tiempo']
-accelz = datosabc['accelz']
+#  Split the data between train and test
+data_ecg, labels_ecg = load_ecg_data(filename)
+training_size = int(params.tamaño_entrenamiento * len(labels_ecg))
+train_data_ecg = data_ecg[:training_size]
+test_data_ecg = data_ecg[training_size:]
+train_labels_ecg = labels_ecg[:training_size]
+test_labels_ecg = labels_ecg[training_size:]
+
+tiempo = data['tiempo'].values
+accelz = data['accelz'].values
 
 # Segmentación de señal por intervalos de tiempo coherentes entre ellos
 # Código guillermo
 
 n = len(tiempo)
 ciclo = np.array([0])
+contador_ciclo = 1
 
 for i in np.arange(n - 1):
     if (tiempo[i + 1] - tiempo[i]) > 0.1 / 60 / 60 / 24:
+        contador_ciclo += 1
         ciclo = np.append(ciclo, [i + 1])
 
-# MRA
-waveletname = 'sym5'
+cont = 0
+while cont <= len(contador_ciclo) and (tecla == ''):
+    signal = accelz[cont]
+    rec = fx.lowpassfilter(signal, params.thresh, params.wavelet_name)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
+    fx.grafica(signal, cont, rec)  # TODO: SE NECESITA REPROGRAMAR LA FUNCIÓN EN 2D PARA CONSIDERAR EL EJE TIEMPO.
 
-for fila_z in accelz:
-    signal_z = accelz[fila_z]
-    coeffs = pywt.wavedec(signal_z, waveletname, level=7)
+    cont += 1
+    tecla = input()
