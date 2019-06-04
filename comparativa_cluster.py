518fbd041bd1c46fa737bf03b0c76187f94e13d3
Ariel Mardones <vmard001@gmail.com>
2019-06-04 12:12:49 -0400

    Definición de tipo y tamaño de cluster en función de métricas ss-ch-db.

--- comparativa_cluster.py
+++ comparativa_cluster.py
@@ -1,16 +1,23 @@
 import pickle
+import numpy as np
 
-list_gof = ['gof Average Clustering.text',
-            'gof Birch.text',
-            'gof Complete Clustering.text',
+list_gof = [#'gof Average Clustering.text',
+            # 'gof Birch.text',
+            #'gof Complete Clustering.text',
             'gof Kmeans clustering.text',
-            'gof Mini Batch Kmeans clustering.text',
-            'gof Single Clustering.text',
-            'gof Spectral Clustering.text',
+            # 'gof Mini Batch Kmeans clustering.text',
+            # 'gof Single Clustering.text',
+            # 'gof Spectral Clustering.text',
             'gof Ward clustering.text']
 
-cluster_gof = []
+cluster_gof = np.array([[2, 3, 4, 5, 6, 7, 8, 9]]).T
 for gof in list_gof:
     with open(gof, 'rb') as f:
-        cluster_gof.append(pickle.load(f))
+        temp = pickle.load(f)
+    cluster_gof = np.hstack([cluster_gof, temp[:, 1:4]])
 
+ss = cluster_gof[:, 1: cluster_gof.shape[1]: 3]
+ch = cluster_gof[:, 2: cluster_gof.shape[1]: 3]
+db = cluster_gof[:, 3: cluster_gof.shape[1]: 3]
+
+print('')
