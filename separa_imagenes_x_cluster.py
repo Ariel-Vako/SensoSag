f743a7f1c1daba3ea6c88d1ea258cb03efe03ce0
Ariel Mardones <vmard001@gmail.com>
2019-06-04 16:15:12 -0400

    Separación de imágenes de señales.

--- separa_imagenes_x_cluster.py
+++ separa_imagenes_x_cluster.py
@@ -1,9 +1,21 @@
 import pickle
 import params
+import shutil
 
 ruta = params.ruta
 pwd_grupos = ruta + f"/clusters - {params.startDate} - {params.endDate} : Size {params.no_cluster}.txt"
 with open(pwd_grupos, 'rb') as fp2:
     ward = pickle.load(fp2)
 
-etiquetas = ward
+etiquetas = ward[5].labels_
+
+for index, labels in enumerate(etiquetas):
+    ruta_imagen = ruta + '/Imágenes/'
+    if index < 406:
+        imagen = f'Ciclo {index}.png'
+    elif 406 <= index < 1500:
+        imagen = f'Ciclo {index + 1}.png'
+    else:
+        imagen = f'Ciclo {index + 2}.png'
+    repositorio = ruta_imagen + f'{labels}/'
+    shutil.move(ruta_imagen + imagen, repositorio + imagen)
