799eb55fd322783d90a3643d1b355a7d6e32fdf9
Ariel Mardones <vmard001@gmail.com>
2019-06-07 18:31:11 -0400

    Data operacional

--- MLP SAG2/extract_data.py
+++ MLP SAG2/extract_data.py
@@ -1,16 +1,27 @@
+__source__ = 'https://matplotlib.org/gallery/api/date.html'
+
 import pandas as pd
 import pickle
 import os
 import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime
+import numpy as np
+
+dia = mdates.DayLocator(interval=1)  # Cada día
+# dia_formato = mdates.DateFormatter('%b %d')
+minutos = mdates.MinuteLocator(interval=10)  # Cada minuto
 
 ruta = os.path.dirname(os.path.realpath(__file__))
 sag2 = ruta + "/SAG2_MLP.txt"
 
 # Lectura y guardado de datos csv a través de Pickle
 if not os.path.isfile(sag2):
+    misssing_values = ['[-11059] No Good Data For Calculation']
     df = pd.read_csv('SAG2 - Octubre2018.csv',
                      parse_dates=['Fecha'],
-                     index_col=['Fecha'])
+                     index_col=['Fecha'],
+                     na_values=misssing_values)
     with open(sag2, 'wb') as f:
         pickle.dump(df, f)
 
@@ -24,21 +35,25 @@ with open(sag2, 'rb') as fp:
 
 
 # create the plot space upon which to plot the data
-fig, ax = plt.subplots(figsize=(10, 10))
+fig, ax = plt.subplots(figsize=(10, 15))
 
 # add the x-axis and the y-axis to the plot
+# date = np.datetime64(df['Fecha']).astype(datetime)
 ax.plot(df.index.values,
         df['REND_2_REAL'])
 
-# rotate tick labels
-plt.setp(ax.get_xticklabels(), rotation=45)
+ax.xaxis.set_major_locator(dia)
+# ax.xaxis.set_major_formatter(dia_formato)
+ax.xaxis.set_minor_locator(minutos)
+ax.grid(b=True, which='major', color='#666666')
+ax.grid(b=True, which='minor', color='#999999', alpha=0.4, linestyle='--')
+ax.minorticks_on()
+
 
 # set title and labels for axes
 ax.set(xlabel="Fecha",
-       ylabel="Temperature (Fahrenheit)",
-       title="Precipitation\nBoulder, Colorado in July 2018")
-
-df.plot(kind='scatter', x='Fecha', y='REND_"_REAL')
+       ylabel="TPH (ton/h)",
+       title="Tonelaje\nMLP, Agosto 2018")
 plt.show()
 
 print('')
