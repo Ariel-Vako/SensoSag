15e914f91b974978640d76d01b3a713a980552ea
Ariel Mardones <vmard001@gmail.com>
2019-06-11 12:43:50 -0400

    Revisión de señales con paquetes perdidos. No serán tomadas en cuenta.

--- señales_incompletas.py
+++ señales_incompletas.py
@@ -4,8 +4,14 @@ import params
 
 señal_bckup = params.ruta + f'/signal- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
 with open(señal_bckup, 'rb') as fp:
-    pc = pickle.load(fp)
+    señal_bckup = pickle.load(fp)
 
 for signal in señal_bckup:
     print(len(signal))
+
+    fig, ax = plt.subplots(figsize=(14, 10))
     plt.plot(signal)
+    ax.grid(b=True, which='major', color='#666666')
+    ax.grid(b=True, which='minor', color='#999999', alpha=0.4, linestyle='--')
+    ax.minorticks_on()
+    plt.show()
