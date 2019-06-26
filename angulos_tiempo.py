fa3f99bef2a1f81f1da073a4c83c0e4d432d94c5
Ariel-Vako <ariel.mardones@bourbaki.cl>
2019-06-26 18:24:11 -0400

    Promedio diario del ángulo del talón y guardado de etiquetas

--- angulos_tiempo.py
+++ angulos_tiempo.py
@@ -24,52 +24,70 @@ def trans_angulo(toes):
     return angle_toe
 
 
-ruta = params.ruta + '/respaldo-datos'
-toe_bckup = ruta + f'/toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
-with open(toe_bckup, 'rb') as fl:
-    all_toe = pickle.load(fl)
+if __name__ == '__main__':
+    ruta = params.ruta + '/respaldo-datos'
+    toe_bckup = ruta + f'/toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+    with open(toe_bckup, 'rb') as fl:
+        all_toe = pickle.load(fl)
 
-all_toe = trans_angulo(all_toe)
+    # all_toe = trans_angulo(all_toe)
+    #
+    # with open(toe_bckup, 'wb') as fp2:
+    #     pickle.dump(all_toe, fp2)
 
-time_toe_bckup = ruta + f'/time_toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
-with open(time_toe_bckup, 'rb') as fl:
-    all_time_toe = pickle.load(fl)
+    # Lectura del tiempo en el cual se encuentra el talón
+    time_toe_bckup = ruta + f'/time_toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+    with open(time_toe_bckup, 'rb') as fl:
+        all_time_toe = pickle.load(fl)
 
-archivo_train_fechas = params.ruta + '/Fechas/fechas.txt'
-with open(archivo_train_fechas, 'rb') as fp:
-    fechas = pickle.load(fp)
+    # Lectura de etiquetas
+    etiquetas = ruta + f'/etiquetas- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+    with open(etiquetas, 'rb') as fp2:
+        labels = pickle.load(fp2)
 
-dia = mdates.DayLocator(interval=2)
-dia_formato = mdates.DateFormatter('%b%d-%H:%M')
-start = 100  # Desde que ciclo se comienza a graficar
-n = 2000  # número de ciclos a graficar
+    # Guardado de etiquetas
+    # ruta = params.ruta + '/respaldo-datos'
+    # etiquetas = ruta + f'/etiquetas- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+    #
+    # with open(etiquetas, 'wb') as fp2:
+    #     pickle.dump(labels, fp2)
 
-fig, ax = plt.subplots(figsize=(12, 8))
-ax.xaxis.set_major_locator(dia)
-ax.xaxis.set_major_formatter(dia_formato)
-ax.set_xlim([fechas[start], fechas[n]])  # - timedelta(days=1)
+    # Lectura de fechas de cada señal
+    archivo_train_fechas = params.ruta + '/Fechas/fechas.txt'
+    with open(archivo_train_fechas, 'rb') as fp:
+        fechas = pickle.load(fp)
 
-ax.grid(b=True, which='major', color='#666666')
-ax.grid(b=True, which='minor', color='#999999', alpha=0.4, linestyle='--')
-ax.xaxis.set_minor_locator(MultipleLocator(1))
-ax.minorticks_on()
-plt.xticks(rotation=45)
+    dia = mdates.DayLocator(interval=2)
+    dia_formato = mdates.DateFormatter('%b%d-%H:%M')
+    start = 100  # Desde que ciclo se comienza a graficar
+    n = 2000  # número de ciclos a graficar
 
-ax.set_ylabel('Angle (°)', fontsize=16)
-ax.set_title(f'Toe Angle', fontsize=18)
+    fig, ax = plt.subplots(figsize=(12, 8))
+    ax.xaxis.set_major_locator(dia)
+    ax.xaxis.set_major_formatter(dia_formato)
+    ax.set_xlim([fechas[start], fechas[n]])  # - timedelta(days=1)
 
-scatter = ax.scatter(fechas[start: n], all_toe[start: n], alpha=0.3)
+    ax.grid(b=True, which='major', color='#666666')
+    ax.grid(b=True, which='minor', color='#999999', alpha=0.4, linestyle='--')
+    ax.xaxis.set_minor_locator(MultipleLocator(1))
+    ax.minorticks_on()
+    plt.xticks(rotation=45)
 
-periodo = 15
-media_movil = sma(all_toe[start: n + periodo], periodo)
+    ax.set_ylabel('Angle (°)', fontsize=16)
+    ax.set_title(f'Toe Angle', fontsize=18)
 
-ax.plot(fechas[start: n], media_movil, c='r')
-plt.show()
-# ax.plot(rec, 'k', label='DWT smoothing', linewidth=2)
-# ax.plot(raw_impacts_, 'r', label='Distance l1', linewidth=2, alpha=0.5)
-# ax.plot(seno2, '--g', label='MCC Robusto', linewidth=1, alpha=0.7)
-# ax.legend()
-# ax.set_title(f'Ángulo: {np.round(toe, 1)} at {np.round(toe_time, 1)}', fontsize=18)
-# ax.set_ylabel('Signal Amplitude', fontsize=16)
-# ax.set_xlabel('Time', fontsize=16)
-# ax.axvspan(inicio, fin - 1, alpha=0.5, color='#98FB98')
+    scatter = ax.scatter(fechas[start: n], all_toe[start: n], alpha=0.3)
+
+    periodo = 15
+    media_movil = sma(all_toe[start: n + periodo], periodo)
+
+    ax.plot(fechas[start: n], media_movil, c='r')
+    plt.show()
+    # ax.plot(rec, 'k', label='DWT smoothing', linewidth=2)
+    # ax.plot(raw_impacts_, 'r', label='Distance l1', linewidth=2, alpha=0.5)
+    # ax.plot(seno2, '--g', label='MCC Robusto', linewidth=1, alpha=0.7)
+    # ax.legend()
+    # ax.set_title(f'Ángulo: {np.round(toe, 1)} at {np.round(toe_time, 1)}', fontsize=18)
+    # ax.set_ylabel('Signal Amplitude', fontsize=16)
+    # ax.set_xlabel('Time', fontsize=16)
+    # ax.axvspan(inicio, fin - 1, alpha=0.5, color='#98FB98')
--- respaldo-datos/promedio_diario_angle_toe.py
+++ respaldo-datos/promedio_diario_angle_toe.py
@@ -1,9 +1,34 @@
 import pickle
 import params
 import numpy as np
+import csv
 
 archivo_train_fechas = params.ruta + '/Fechas/fechas.txt'
 with open(archivo_train_fechas, 'rb') as fp:
     fechas = pickle.load(fp)
 
-promedio_diario = []
+ruta = params.ruta + '/respaldo-datos'
+toe_bckup = ruta + f'/toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+with open(toe_bckup, 'rb') as fl:
+    all_toe = pickle.load(fl)
+
+# all_toe = at.trans_angulo(all_toe)
+diccionario_promedio_angulo = {}
+j = 0
+while j < len(fechas):
+    date = fechas[j].date()
+    promedio_diario = [all_toe[j]]
+    while date == fechas[j + 1].date():
+        promedio_diario.append(all_toe[j + 1])
+        j += 1
+        if j + 1 >= len(fechas):
+            break
+    j += 1
+    alfa_medio = np.round(np.average(promedio_diario), 2)
+    diccionario_promedio_angulo[f'{date} 19:00:00'] = alfa_medio
+
+with open(f'{ruta}/test.csv', 'w') as f:
+    w = csv.writer(f)
+    for key, value in diccionario_promedio_angulo.items():
+        w.writerow([key, value])
+print('')
