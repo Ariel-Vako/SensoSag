93014b02dccf425ceca9461a1eb675b7cdcacf10
Ariel-Vako <ariel.mardones@bourbaki.cl>
2019-06-26 19:35:31 -0400

    Estadísticos por cluster

---
+++ statistics_angle_per_cluster.py
@@ -0,0 +1,22 @@
+import params
+import pickle
+import numpy as np
+
+# Lectura de los ángulos del talón
+ruta = params.ruta + '/respaldo-datos'
+toe_bckup = ruta + f'/toes- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+with open(toe_bckup, 'rb') as fl:
+    all_toe = pickle.load(fl)
+
+# Lectura de etiquetas
+etiquetas = ruta + f'/etiquetas- {params.startDate} - {params.endDate}: {params.cantidad}.txt'
+with open(etiquetas, 'rb') as fp2:
+    labels = pickle.load(fp2)
+
+estadísticos_per_cluster = {}
+for i in range(5):
+    index = np.where(labels == i)
+    sujeto = [all_toe[i] for i in index[0]]
+    promedio = np.average(sujeto)
+    desv_st = np.std(sujeto)
+    estadísticos_per_cluster[f'{i}'] = {'promedio': promedio, 'std': desv_st}
